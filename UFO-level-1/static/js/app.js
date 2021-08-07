45efe7d72fff3d7a0a6e88bc1209a347fb2e318f
mjyurich <matthewyurich@gmail.com>
2021-08-07 14:54:00 -0400

    Data added to HTML

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,40 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+console.log(data);
+
+data.forEach((alienSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(alienSighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+//Select the Button
+var button = d3.select("filter-btn");
+
+//Select the form
+var form = d3.select("#form");
+
+//Create Event Handlers
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+//Complete the event handler function for the form
+function runEnter() {
+
+    //Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    //Select the input element and get the raw HTML node
+    var inputElement = d3.select("#form-control");
+
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(people);
+}
+
